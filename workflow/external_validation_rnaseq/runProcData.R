fadff20f9ec20ae5f83b21c4b7cafb5da8166f90
Farnoosh Babazadeh <farnooshbabazadeh@gmail.com>
2026-02-06 14:41:56 -0500

    add script to convert mae object to se for io cohorts

--- workflow/external_validation_rnaseq/runProcData.R
+++ workflow/external_validation_rnaseq/runProcData.R
@@ -1,12 +1,11 @@
 # -----------------------------------------------------------
-# Processed Data Creation Script
+# Description:
 # This script reads input and output directory paths and study metadata
 # from `config_proc.yaml`, and performs the following:
 #
 #   - Loads raw MultiAssayExperiment (MAE) object from `data/rawdata`
 #   - Converts MAE to SummarizedExperiment (SE)
 #   - Loads gene signature data and metadata
-#   - Saves a combined processed data object to `data/procdata`
 #
 # Config file: config/config_proc.yaml
 # -----------------------------------------------------------
@@ -41,27 +40,3 @@ treatment_type <- config$treatment_type # "PD-(L)1"  (Other options include: CTL
 mae_obj <- readRDS(file.path(dir_in, "ICB_Gide.rds"))  # MultiAssayExperiment
 se_obj  <- PredictioR::createSE(mae_obj)  # Extract SummarizedExperiment (TPM + clinical)
 
-########################################################
-## Load Gene Signature Data
-########################################################
-# Signature objects should be pre-curated and saved as .rda files
-load(file.path(dir_in, "signature.rda"))   # signature matrix
-load(file.path(dir_in, "sig.info.rda"))    # metadata for signatures
-
-dat <- list('ICB' = dat_icb,
-            'signature' = signature,
-            'sig.info' = sig.info)
-
-########################################################
-## Combine Data into a Named List and Save
-########################################################
-dat <- list(
-  ICB       = se_obj,
-  signature = signature,
-  sig.info  = sig.info
-)
-
-save(
-  dat, 
-  file = file.path(dir_out, paste0(paste(study_icb, cancer_type, treatment_type, sep = "__"), ".rda"))
-)
